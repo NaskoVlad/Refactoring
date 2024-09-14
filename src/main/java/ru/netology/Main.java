1ea36cfe5174d71fcf3e39cc4d11a0bfab0bd93a
Vlad test <taranovivan666@gmail.com>
2024-09-14 13:58:47 +0300

    Задание 1

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,18 @@
+package ru.netology;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.logging.Handler;
+
+public class Main {
+    public static void main(String[] args) {
+      final var server = new Server();
+      server.start();
+    }
+}
+
+
